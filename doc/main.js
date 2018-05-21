6a463ba171cc0dc269b7c2cceb00405f03799b13
YuanBingrui <bingrui_yuan0721@163.com>
2018-05-21 23:05:05 +0800

    add vue-markdown-loader

--- doc/main.js
+++ doc/main.js
@@ -2,13 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import xbjUI from '../src/index.js'
 import VueRouter from 'vue-router'
 import routes from './route.config'
 
 Vue.config.productionTip = false
 
-Vue.use(xbjUI)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
